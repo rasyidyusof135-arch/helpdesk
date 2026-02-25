a8b2089cd48857c709e68db217e8462bf79bb27d
rasyidyusof135-arch <rasyidyusof@example.com>
2026-02-26 01:29:13 +0800

    Update db_connect.php to use environment variables

--- db_connect.php
+++ db_connect.php
@@ -1,11 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "it_helpdesk";
+$servername = getenv("MYSQLHOST") ?: "localhost";
+$username = getenv("MYSQLUSER") ?: "root";
+$password = getenv("MYSQLPASSWORD") ?: "";
+$dbname = getenv("MYSQLDATABASE") ?: "it_helpdesk";
+$port = getenv("MYSQLPORT") ?: 3306;
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Check connection
 if ($conn->connect_error) {
